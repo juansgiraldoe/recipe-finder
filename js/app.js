b04c3554df5d9b42b77af860edc05340557bdcc9
Juan Sebastian Giraldo <juans.giraldo.e@gmail.com>
2024-03-03 13:05:06 -0500

    [Update] - Show categories in DOM.

--- js/app.js
+++ js/app.js
@@ -1,13 +1,22 @@
 function iniciarApp() {
+  const categoriasSelect = document.querySelector('#categorias')
   obtenerCategorias();
 
   function obtenerCategorias() {
     const url = 'https://www.themealdb.com/api/json/v1/1/categories.php';
     fetch(url)
       .then(res => res.json())
-      .then(res => {
+      .then(res => mostrarCategorias(res.categories));
+  };
 
-      });
+  function mostrarCategorias(categorias = []) {
+    categorias.forEach(categoria => {
+      const option = document.createElement('OPTION');
+      const {strCategory} = categoria;
+      option.value = strCategory;
+      option.textContent = strCategory;
+      categoriasSelect.appendChild(option)
+    });
   };
 };
 document.addEventListener('DOMContentLoaded', iniciarApp);
