129720b78383cc93a67f9d9357a43b65a336bc9d
Juan Sebastian Giraldo <juans.giraldo.e@gmail.com>
2024-03-04 16:17:52 -0500

    [Update] - Show all the modal info.

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ function iniciarApp() {
   categoriasSelect.addEventListener('change', seleccionarCategoria)
   
   const resultado = document.querySelector('#resultado');
-
+  const modal = new bootstrap.Modal('#modal', {})
   obtenerCategorias();
 
   function obtenerCategorias() {
@@ -62,7 +62,7 @@ function iniciarApp() {
 
       const recetaBtn = document.createElement('BUTTON');
       recetaBtn.classList.add('btn', 'btn-danger', 'w-100');
-      recetaBtn.textContent = 'Ver receta.';
+      recetaBtn.textContent = 'Ver receta';
       // recetaBtn.dataset.bsTarget = '#modal';
       // recetaBtn.dataset.bsToggle = 'modal';
       recetaBtn.onclick = function () {
@@ -84,8 +84,41 @@ function iniciarApp() {
 
   function seleccionarReceta(id) {
     const url = `https://themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
-    
+    fetch(url)
+      .then(res => res.json())
+      .then(res => mostrarInfoModal(res.meals[0]));
   };
+
+  function mostrarInfoModal(receta) {
+    const { idMeal, strInstructions, strMeal, strMealThumb } = receta;
+    
+    const modalTitle = document.querySelector('.modal .modal-title');
+    modalTitle.textContent = strMeal;
+    const modalBody = document.querySelector('.modal .modal-body');
+    modalBody.textContent = strInstructions;
+    modalBody.innerHTML = `
+      <img class="img-fluid" src="${strMealThumb}" alt="Imagen de la receta ${strMeal}">
+      <h3 class="my-3">Instrucciones.</h3>
+      <p>${strInstructions}</p>
+      <h3 class="my-3">Cantidades e ingredientes.</h3>
+    `;
+    const listGroup = document.createElement('UL');
+    listGroup.classList.add('list-group');
+    
+    for (let i = 1; i <= 20; i++) {
+      if (receta[`strIngredient${i}`]) {
+        const ingrediente = receta[`strIngredient${i}`];
+        const cantidad = receta[`strMeasure${i}`];
+        const ingredienteLi = document.createElement('LI');
+        ingredienteLi.classList.add('list-group-item');
+        ingredienteLi.textContent = `${ingrediente} - ${cantidad}`;
+        listGroup.appendChild(ingredienteLi)
+      };
+    };
+    modalBody.appendChild(listGroup)
+    //Mostrar modal.
+    modal.show();
+  }
 
   function limpiarHtml(selector) {
     while (selector.firstChild) {
